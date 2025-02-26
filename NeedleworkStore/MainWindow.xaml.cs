dd0525d4d7264cf9f0093cff818e3f56743b0c1c
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-02-26 13:36:03 +0300

    Разбор сложных элементов XAML и использование объектов класса С# в XAML

--- NeedleworkStore/MainWindow.xaml.cs
+++ NeedleworkStore/MainWindow.xaml.cs
@@ -18,6 +18,17 @@ namespace NeedleworkStore
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    /// 
+
+    public class Product {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public override string ToString()
+        {
+            return $"title: {Title}, price: {Price}";
+        }
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -28,6 +39,8 @@ namespace NeedleworkStore
             btn.Height = 30;
             btn.Content = "3";
             LayOut.Children.Add(btn);
+            btn.HorizontalAlignment = HorizontalAlignment.Right;
+            btn.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Aqua);
         }
 
         private void btnStart_Click(object sender, RoutedEventArgs e)
