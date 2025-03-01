39182617a107d20806730bd71472ad2e62f135bc
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-03-01 05:41:46 +0300

    Компановка, StackPanel, WrapPanel, DockPanel, кнопка сложно свойство

--- NeedleworkStore/MainWindow.xaml.cs
+++ NeedleworkStore/MainWindow.xaml.cs
@@ -20,34 +20,13 @@ namespace NeedleworkStore
     /// </summary>
     /// 
 
-    public class Product {
-        public string Title { get; set; }
-        public int Price { get; set; }
-        public override string ToString()
-        {
-            return $"title: {Title}, price: {Price}";
-        }
-    }
-
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
-            Button btn = new Button();
-            btn.Width = 100;
-            btn.Height = 30;
-            btn.Content = "3";
-            LayOut.Children.Add(btn);
-            btn.HorizontalAlignment = HorizontalAlignment.Right;
-            btn.Background = new System.Windows.Media.SolidColorBrush(System.Windows.Media.Colors.Aqua);
+            InitializeComponent();           
         }
-
-        private void btnStart_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Ok");
-            MessageBox.Show("Hello");
-        }
-
+               
     }
 }
