9c661a9c8bddb4bdabecbdfbc39ab85d0acdcf6b
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-03-10 11:26:12 +0300

    add validation

---
+++ NeedleworkStore/Classes/CheckValidation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedleworkStore.Classes
+{
+    static internal class CheckValidation
+    {      
+        public static bool CheckLogin(string txt)
+        {
+            foreach (char ch in txt)
+            {
+                if (ch == '&' || ch == '+' || ch == '=' || ch == '<' || ch == '>' || ch == ',' || ch == '-' || ch == '"' || ch == '\'')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool CheckEmptyNull(string txt)
+        {
+            if (!String.IsNullOrEmpty(txt) && !String.IsNullOrWhiteSpace(txt))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- NeedleworkStore/Pages/AuthPage.xaml.cs
+++ NeedleworkStore/Pages/AuthPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NeedleworkStore.Classes;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NeedleworkStore.Pages
 {
@@ -79,13 +71,13 @@ namespace NeedleworkStore.Pages
         }
 
         private void btnSignIn_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Hello!");
-        }
-
-        private void chbT1_Checked(object sender, RoutedEventArgs e)
-        {
-            chbT2.IsChecked = false;
+        {           
+            if (!CheckValidation.CheckEmptyNull(txtLog.Text) || !CheckValidation.CheckEmptyNull(txtPass.Password))
+            {
+                MessageBox.Show("Все поля должны быть заполнены!");
+                return;
+            }
+            this.NavigationService.Navigate(new ProductsPage());
         }
     }
 }
