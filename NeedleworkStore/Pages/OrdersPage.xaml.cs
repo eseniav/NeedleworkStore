7b95ef8118a1daa4564076357dc735cb446b17bb
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-04-09 13:44:36 +0300

    new pages and model files

---
+++ NeedleworkStore/Pages/OrdersPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using NeedleworkStore.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NeedleworkStore.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для OrdersPage.xaml
+    /// </summary>
+    public partial class OrdersPage : Page
+    {
+        public OrdersPage()
+        {
+            InitializeComponent();
+        }
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.NavigationService.CanGoForward)
+            {
+                this.NavigationService.GoForward();
+            }
+            else
+            {
+                MessageBox.Show("No page to go forward");
+            }
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.NavigationService.CanGoBack)
+            {
+                this.NavigationService.GoBack();
+            }
+            else
+            {
+                MessageBox.Show("No page to go back");
+            }
+        }
+        private void btnCart_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new CartPage());
+        }
+        private void btnShop_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Переход на страницу О магазине");
+        }
+
+        private void btnProfile_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new ProfilePage());
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new AuthPage());
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (CheckValidation.CheckEmptyNull(txtSearch.Text))
+            {
+                MessageBox.Show("Переход на страницу с найденной информацией\n" +
+                "или подсвеченная информация на этой странице");
+            }
+            else
+            {
+                MessageBox.Show("Заполните поле!");
+            }
+        }
+        private void cmbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            MessageBox.Show("Соответствующая сортировка");
+        }
+    }
+}
