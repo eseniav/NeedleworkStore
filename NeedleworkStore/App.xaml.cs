3ac095e17b6492c2e4a2f29a62cc69784299f9f1
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-04-11 10:46:11 +0300

    store db context

--- NeedleworkStore/App.xaml.cs
+++ NeedleworkStore/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using NeedleworkStore.AppData;
 
 namespace NeedleworkStore
 {
@@ -13,5 +14,6 @@ namespace NeedleworkStore
     /// </summary>
     public partial class App : Application
     {
+        public static NeedleworkStoreEntities ctx = new NeedleworkStoreEntities();
     }
 }
