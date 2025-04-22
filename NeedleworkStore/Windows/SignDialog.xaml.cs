4575e6f60d0c5575c398c606e4759a80d16c73d4
viktor <sterilework@gmail.com>
2025-04-22 17:21:53 +0300

    feat: add sign dialog window

---
+++ NeedleworkStore/Windows/SignDialog.xaml.cs
@@ -0,0 +1,42 @@
+﻿using NeedleworkStore.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NeedleworkStore.Windows
+{
+    /// <summary>
+    /// Interaction logic for SignDialog.xaml
+    /// </summary>
+    public partial class SignDialog : Window
+    {
+        public SignDialog()
+        {
+            InitializeComponent();
+        }
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.frame.Navigate(new AuthPage());
+            this.Close();
+        }
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.frame.Navigate(new RegistrationPage());
+            this.Close();
+        }
+    }
+}
