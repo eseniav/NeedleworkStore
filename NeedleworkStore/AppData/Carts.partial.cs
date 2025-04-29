2ae476a2479bd099c72c17a4752dd20699b8ebf6
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-04-29 14:02:42 +0300

    add new class for +-button

---
+++ NeedleworkStore/AppData/Carts.partial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedleworkStore.AppData
+{
+    public partial class Carts : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public int Quantity 
+        {
+            get => QuantityCart;
+            set 
+            {
+                if (QuantityCart != value)
+                {
+                    QuantityCart = value;
+                    OnPropertyChanged(nameof(Quantity));
+                    OnPropertyChanged(nameof(QuantityCart)); 
+                }
+            }
+        }
+    }
+}
