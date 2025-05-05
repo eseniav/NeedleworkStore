f2ec12c08c1f00eb10129278c7e154a83ea51c65
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-05-05 23:50:02 +0300

    add show hide pass

--- NeedleworkStore/Pages/ProfilePage.xaml.cs
+++ NeedleworkStore/Pages/ProfilePage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Application = System.Windows.Application;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace NeedleworkStore.Pages
 {
@@ -29,7 +31,6 @@ namespace NeedleworkStore.Pages
             Dictionary<string, string> profileData = new Dictionary<string, string>
             {
                 { "txtLog", user.Login },
-                { "txtPass", user.Password },
                 { "txtEmail", user.UserEmail },
                 { "txtLastname", user.UserLastname },
                 { "txtFirstname", user.UserName },
@@ -69,6 +70,10 @@ namespace NeedleworkStore.Pages
         private void Image_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.NavigationService.Navigate(new OneProductPage());
+        }
+        private void chbShowPass_Click(object sender, RoutedEventArgs e)
+        {
+            txtPass.Text = (bool)chbShowPass.IsChecked ? user.Password : "********";
         }
     }
 }
