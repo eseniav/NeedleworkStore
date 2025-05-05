093c35115d54f8f3f91fbab4cc04474c16f8d324
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-05-05 03:57:26 +0300

    add saving order information to db

--- NeedleworkStore/Pages/OrderRegistrationPage.xaml.cs
+++ NeedleworkStore/Pages/OrderRegistrationPage.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NeedleworkStore.Classes;
+using System.Data.Entity;
 
 namespace NeedleworkStore.Pages
 {
@@ -27,6 +29,7 @@ namespace NeedleworkStore.Pages
         List<PickUpPoints> pickUpPoints;
         Cities selectedCity;
         PickUpPoints selectedPickUpPoint;
+        MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
         public OrderRegistrationPage(List<Carts> crt)
         {
             InitializeComponent();
@@ -116,10 +119,115 @@ namespace NeedleworkStore.Pages
         {
             DelOneProduct((Carts)((Button)sender).DataContext);
         }
-
+        public bool ContainsOnlyDigits(string input) => !string.IsNullOrEmpty(input) && input.All(char.IsDigit);
+        public bool IsValidMonth(string monthText)
+        {
+            if (!ContainsOnlyDigits(monthText) || monthText.Length != 2)
+                return false;
+            int month = int.Parse(monthText);
+            return month >= 1 && month <= 12;
+        }
+        public bool IsValidYear(string yearText)
+        {
+            if (!ContainsOnlyDigits(yearText) || yearText.Length != 2)
+                return false;
+            int year = int.Parse(yearText);
+            return year >= DateTime.Now.Year % 100;
+        }
+        public bool IsNotExpired(string monthText, string yearText)
+        {
+            if (!IsValidMonth(monthText) || !IsValidYear(yearText))
+                return false;
+            int year = int.Parse(yearText);
+            int currentYear = DateTime.Now.Year % 100;
+            if (year > currentYear)
+                return true;
+            return int.Parse(monthText) >= DateTime.Now.Month;
+        }
+        private bool CheckConditions()
+        {
+            if (cmbPickUpPointCity.SelectedItem == null || cmbPickUpPointAddress.SelectedItem == null || cmbPayment.SelectedItem == null)
+                return false;
+            if (cmbPayNow.IsSelected == true)
+            {
+                if (!ContainsOnlyDigits(txtBCardNum.Text) || txtBCardNum.Text.Length < 16)
+                    return false;
+                if (!IsNotExpired(txtBMonth.Text, txtBYear.Text))
+                    return false;
+            }
+            return true;
+        }
         private void btnPOrderReg_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Оформление заказа");
+             if (!CheckConditions())
+            {
+                string errorMessage = "Не удалось оформить заказ.\n";
+                if (cmbPickUpPointCity.SelectedItem == null)
+                    errorMessage += "• Не выбран город\n";
+                if (cmbPickUpPointAddress.SelectedItem == null)
+                    errorMessage += "• Не выбран пункт выдачи\n";
+                if (cmbPayment.SelectedItem == null)
+                    errorMessage += "• Не выбран способ оплаты\n";
+                if (cmbPayNow.IsSelected == true && !ContainsOnlyDigits(txtBCardNum.Text))
+                    errorMessage += "• Номер карты должен содержать только цифры\n";
+
+                MessageBox.Show(
+                    errorMessage + "\nПожалуйста, заполните все поля правильно",
+                    "Проверьте данные",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning
+                );
+                return;
+            }
+             try
+            {
+                int paymentID = cmbPayNow.IsSelected ? 1 : 2;
+                Orders newOrder = new Orders
+                {
+                    UserID = (int)mainWindow.UserID,
+                    PickUpPointID = selectedPickUpPoint.PickUpPointID,
+                    FormationDate = DateTime.Now,
+                };
+                App.ctx.Orders.Add(newOrder);
+                foreach (Carts list in orderCart)
+                {
+                    OrderCompositions newOrderComposition = new OrderCompositions
+                    {
+                        Quantity = list.QuantityCart,
+                        ProductID = list.ProductID,
+                        OrderID = newOrder.OrderID,
+                        OrderPrice = (int)list.SumProducts,
+                    };
+                    App.ctx.OrderCompositions.Add(newOrderComposition);
+                }
+                AssigningStatuses newAssigningStatuses = new AssigningStatuses
+                {
+                    PaymentStatusID = paymentID,
+                    ReceivingStatusID = 2,
+                    ModifiedDate = DateTime.Now,
+                    OrderID = newOrder.OrderID,
+                    ProcessingStatusID = 1,
+                };
+                App.ctx.AssigningStatuses.Add(newAssigningStatuses);
+                App.ctx.SaveChanges();
+                MessageBox.Show(
+                            "✅ Ваш заказ успешно оформлен!\n\n" +
+                            $"Номер заказа: #{newOrder.OrderID}\n" +
+                            "Чек в PDF-формате доступен для скачивания:\n" +
+                            "1. Откройте «Профиль»\n" +
+                            "1. Перейдите в раздел «Заказы»\n" +
+                            "2. Найдите текущий заказ\n" +
+                            "3. Нажмите кнопку «Скачать чек»\n\n" +
+                            "Спасибо за покупку!",
+                            "Заказ подтверждён",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Information
+                        );
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
         private void SetPickUpPointAddress(string address)
         {
