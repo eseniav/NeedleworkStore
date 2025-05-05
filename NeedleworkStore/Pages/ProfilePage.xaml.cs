fc0f896b64521f89bf5bf287eaf5524163a91bb8
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-05-05 22:11:53 +0300

    bind profile to db

--- NeedleworkStore/Pages/ProfilePage.xaml.cs
+++ NeedleworkStore/Pages/ProfilePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using NeedleworkStore.Classes;
+﻿using NeedleworkStore.AppData;
+using NeedleworkStore.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,34 +22,36 @@ namespace NeedleworkStore.Pages
     /// </summary>
     public partial class ProfilePage : Page
     {
+        MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+        Users user;
+        private void SetProfileValues()
+        {
+            Dictionary<string, string> profileData = new Dictionary<string, string>
+            {
+                { "txtLog", user.Login },
+                { "txtPass", user.Password },
+                { "txtEmail", user.UserEmail },
+                { "txtLastname", user.UserLastname },
+                { "txtFirstname", user.UserName },
+                { "txtPatr", user.UserPatronymic },
+                { "txtBirthDate", user.Birthday?.ToString("dd.MM.yyyy") },
+                { "txtPhone", user.UserPhone },
+            };
+            string defaultText = "Данные не указаны";
+            SolidColorBrush defaultColor = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#852614"));
+            foreach (var item in profileData)
+            {
+                TextBlock textBlock = (TextBlock)FindName(item.Key);
+                textBlock.Text = string.IsNullOrEmpty(item.Value) ? defaultText : item.Value;
+                textBlock.Foreground = string.IsNullOrEmpty(item.Value) ? defaultColor : textBlock.Foreground;
+            }
+        }
         public ProfilePage()
         {
             InitializeComponent();
+            user = App.ctx.Users.FirstOrDefault(u => u.UserID == mainWindow.UserID);
+            SetProfileValues();
         }
-        private void btnForward_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.NavigationService.CanGoForward)
-            {
-                this.NavigationService.GoForward();
-            }
-            else
-            {
-                MessageBox.Show("No page to go forward");
-            }
-        }
-
-        private void btnBack_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.NavigationService.CanGoBack)
-            {
-                this.NavigationService.GoBack();
-            }
-            else
-            {
-                MessageBox.Show("No page to go back");
-            }
-        }       
-
         private void btnFav_Click(object sender, RoutedEventArgs e)
         {
             this.NavigationService.Navigate(new FavorPage());
