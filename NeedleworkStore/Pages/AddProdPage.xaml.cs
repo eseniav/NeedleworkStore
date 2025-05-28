b6115a353772b6c84ee18ee38ea8f9d51f5b9235
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-05-28 11:56:16 +0300

    bind prod with add prod page

--- NeedleworkStore/Pages/AddProdPage.xaml.cs
+++ NeedleworkStore/Pages/AddProdPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using NeedleworkStore.UCElements;
+﻿using NeedleworkStore.AppData;
+using NeedleworkStore.UCElements;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,11 +23,29 @@ namespace NeedleworkStore.Pages
     /// </summary>
     public partial class AddProdPage : Page
     {
+        Products prod;
         public ProductFilterViewModel FilterVM { get; set; } = new ProductFilterViewModel();
+        public void SetProduct()
+        {
+            txtAddNameProd.Text = prod.ProductName;
+            txtbAddPriceProd.Text = prod.ProductPrice.ToString();
+            txtbAdddescriptionProd.Text = prod.Description;
+            txtbQR.Text = prod.QRLink;
+            if(prod.AvailabilityStatusID == 1)
+                cmbAvailYes.IsSelected = true;
+            else
+                cmbAvailNo.IsSelected = true;
+        }
         public AddProdPage(MyProducts selectedProduct = null)
         {
             InitializeComponent();
             DataContext = this;
+            prod = selectedProduct;
+            if (prod != null)
+            {
+                SetProduct();
+                return;
+            }
         }
     }
 }
