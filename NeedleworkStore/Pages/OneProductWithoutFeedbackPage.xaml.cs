d39f5b190015261c129ccb8fb0f0ce2bd0420463
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-05-30 22:55:16 +0300

    del back/forward logic buttons

--- NeedleworkStore/Pages/OneProductWithoutFeedbackPage.xaml.cs
+++ NeedleworkStore/Pages/OneProductWithoutFeedbackPage.xaml.cs
@@ -97,30 +97,6 @@ namespace NeedleworkStore.Pages
                 label.Foreground = string.IsNullOrEmpty(item.Value) ? defaultColor : label.Foreground;
             }
         }
-        private void btnForward_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.NavigationService.CanGoForward)
-            {
-                this.NavigationService.GoForward();
-            }
-            else
-            {
-                MessageBox.Show("No page to go forward");
-            }
-        }
-
-        private void btnBack_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.NavigationService.CanGoBack)
-            {
-                this.NavigationService.GoBack();
-            }
-            else
-            {
-                MessageBox.Show("No page to go back");
-            }
-        }        
-
         private void btnCartIn_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Добавляет товар в корзину");
