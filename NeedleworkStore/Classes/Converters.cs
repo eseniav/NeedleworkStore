1cdcbb011c42de38c629c5371dcbb8c2056b1278
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-05-30 13:55:03 +0300

    add image path converter

---
+++ NeedleworkStore/Classes/Converters.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NeedleworkStore.Classes
+{
+    public class ImagePathConverter : IValueConverter
+    {
+        private const string BaseResourcePath = "/ProdImages/";
+        private const string DefaultImage = "/ResImages/NoPicture.png";
+        private static readonly string ProjectRootPath = GetProjectRootPath();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string imgName) || string.IsNullOrWhiteSpace(imgName))
+                return DefaultImage;
+            string resourcePath = BaseResourcePath + imgName;
+            if (ResourceExists(resourcePath))
+                return resourcePath;
+            string filePath = System.IO.Path.Combine(ProjectRootPath, "ProdImages", imgName);
+            if (File.Exists(filePath))
+                return filePath;
+            return DefaultImage;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+        private bool ResourceExists(string resourcePath)
+        {
+            try
+            {
+                var resource = Application.GetResourceStream(new Uri(resourcePath, UriKind.Relative));
+                return resource != null;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private static string GetProjectRootPath()
+        {
+            string exePath = AppDomain.CurrentDomain.BaseDirectory;
+            DirectoryInfo directory = new DirectoryInfo(exePath);
+            for (int i = 0; i < 2; i++)
+            {
+                if (directory.Parent != null)
+                    directory = directory.Parent;
+            }
+            return directory.FullName;
+        }
+    }
+}
