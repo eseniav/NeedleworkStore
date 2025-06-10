552596775cbd441b6e390ed068bf150ccbac480d
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-06-10 03:44:09 +0300

    add check password

--- NeedleworkStore/Classes/CheckValidation.cs
+++ NeedleworkStore/Classes/CheckValidation.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace NeedleworkStore.Classes
 {
@@ -19,13 +22,59 @@ namespace NeedleworkStore.Classes
             }
             return true;
         }
-        public static bool CheckEmptyNull(string txt)
+        public static ValidationState CheckLogin(TextBox txtBLogin)
         {
-            return !String.IsNullOrEmpty(txt) && !String.IsNullOrWhiteSpace(txt);
+            ValidationState state = new ValidationState();
+            if (!CheckValidation.CheckEmptyNull(txtBLogin.Text))
+                state.SetError("Поле обязательно для заполнения");
+            else if (!CheckValidation.CheckLogin(txtBLogin.Text))
+                state.SetError("Поле заполнено неправильно");
+            else if (App.ctx.Users.FirstOrDefault(u => u.Login == txtBLogin.Text) != null)
+                state.SetError("Введенный логин уже используется");
+            return state;
         }
-        public static bool CheckInt(string txt)
-        {            
-            return Int32.TryParse(txt.Trim(), out int a);
+        public static bool CheckPass(string txt)
+        {
+            if (txt.Length < 8 || txt.Length > 50)
+                return false;
+            bool hasCapital = false;
+            bool hasNumber = false;
+            foreach (char ch in txt)
+            {
+                if (char.IsUpper(ch))
+                    hasCapital = true;
+                else if (char.IsDigit(ch))
+                    hasNumber = true;
+                if (hasCapital && hasNumber)
+                    return true;
+            }
+            return hasCapital && hasNumber;
+        }
+        public  static ValidationState CheckPassword(PasswordBox psxB)
+        {
+            ValidationState state = new ValidationState();
+            if (!CheckValidation.CheckEmptyNull(psxB.Password))
+                state.SetError("Поле обязательно для заполнения");
+            else if (!CheckValidation.CheckPass(psxB.Password))
+                state.SetError("Поле заполнено неправильно");
+            return state;
+        }
+        public static bool CheckEmptyNull(string txt) => !String.IsNullOrEmpty(txt) && !String.IsNullOrWhiteSpace(txt);
+        public static bool CheckInt(string txt) => Int32.TryParse(txt.Trim(), out int a);
+    }
+    public class ValidationState
+    {
+        public bool IsError { get; set; }
+        public string ErrorMessage { get; set; }
+        /// <summary>
+        /// Устанавливает состояние ошибки с указанным сообщением.
+        /// </summary>
+        /// <param name="s">Сообщение об ошибке, которое будет сохранено в свойстве ErrorMessage.
+        /// Если передана пустая строка или null, состояние ошибки не изменяется.</param>
+        public void SetError(string s)
+        {
+            ErrorMessage = s;
+            IsError = true;
         }
     }
 }
--- NeedleworkStore/Pages/RegistrationPage.xaml.cs
+++ NeedleworkStore/Pages/RegistrationPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using NeedleworkStore.AppData;
+using NeedleworkStore.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,9 +34,31 @@ namespace NeedleworkStore.Pages
             txtBlReqFields.Text = "Поля с * обязательны для заполнения";
             mainWindow.btnProd.IsEnabled = true;
         }
-        
+        private void ColorInputControl(Control control, bool isError)
+        {
+            if (control == null)
+                throw new ArgumentNullException(nameof(control));
+            var brush = isError
+                ? new SolidColorBrush(Color.FromArgb(100, 251, 174, 210))
+                : new SolidColorBrush(Colors.White);
+            control.Background = brush;
+        }
+        public bool CheckLoginError()
+        {
+            ValidationState state = CheckValidation.CheckLogin(txtBLogin);
+            ColorInputControl(txtBLogin, state.IsError);
+            errorLog.Text = state.ErrorMessage;
+            return state.IsError;
+        }
         private void btnReg_Click(object sender, RoutedEventArgs e)
         {
+            CheckLoginError();
+            CheckPassError();
+            if (CheckLoginError() || CheckPassError())
+            {
+                MessageBox.Show("Проверьте форму на ошибки!", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
             try
             {
                 Users newUser = new Users
@@ -58,6 +81,21 @@ namespace NeedleworkStore.Pages
             {
                 MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+        }
+        public bool CheckPassError()
+        {
+            ValidationState state = CheckValidation.CheckPassword(psxB);
+            ColorInputControl(psxB, state.IsError);
+            errorPass.Text = state.ErrorMessage;
+            return state.IsError;
+        }
+        private void txtBLogin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CheckLoginError();
+        }
+        private void psxB_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            CheckPassError();
         }
     }
 }
