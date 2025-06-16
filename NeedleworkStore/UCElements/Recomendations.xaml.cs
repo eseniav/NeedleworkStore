be5c0954a839b690c1fcc822f97e8bf284afeb85
Yeseniya Vishnyakova <esenia-v@yandex.ru>
2025-06-16 10:20:24 +0300

    binding images

---
+++ NeedleworkStore/UCElements/Recomendations.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using NeedleworkStore.AppData;
+
+namespace NeedleworkStore.UCElements
+{
+    /// <summary>
+    /// Логика взаимодействия для Recomendations.xaml
+    /// </summary>
+    public partial class Recomendations : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private List<Products> products;
+        public List<Products> ProductsList {
+            get => products;
+            set {
+                products = value;
+                OnPropertyChanged(nameof(ProductsList));
+            }
+        }
+        public Recomendations()
+        {
+            InitializeComponent();
+            ProductsList = App.ctx.Products.Take(3).ToList();
+            DataContext = this;
+        }
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Переход на страницу с этим товаром");
+        }
+        private void btnCartIn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Добавляет товар в корзину");
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += (s, args) =>
+            {
+                //ppCartIn.IsOpen = false;
+                timer.Stop();
+            };
+
+            //ppCartIn.IsOpen = true;
+            timer.Start();
+        }
+        private void btnFavor_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Добавляет товар в избранное");
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += (s, args) =>
+            {
+                //ppFavorIn.IsOpen = false;
+                timer.Stop();
+            };
+
+            //ppFavorIn.IsOpen = true;
+            timer.Start();
+        }
+    }
+}
